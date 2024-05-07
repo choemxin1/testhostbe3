da58e6318186f4bae321d5d53d5cf008d3d72d11
choemxin1 <chotaotaodkm3@gmail.com>
2024-05-07 11:42:59 +0700

    first commit

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -3,9 +3,9 @@ import CRUDService from "../services/CRUDService";
 
 let getHomePage = async (req, res) => {
     try {
-        // let data = await db.User.findAll();
+         let data = await db.User.findAll();
         return res.render('homepage.ejs', {
-            data: JSON.stringify({})
+            data: JSON.stringify(data)
         });
     } catch (e) {
         console.log(e)
